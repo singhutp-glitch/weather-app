81c83f792e4e25195f2b8eb46470fde6a8d95179
singhutp-glitch <singhutp@gmail.com>
2025-10-27 12:48:12 +0530

    Make weather data processor

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ async function getWeather()
 {
     const response=await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=3F26HRSJQUHVH5X589RRYHJVR');
     const data=await response.json();
-    console.log(data.description);
+    return data;
 }
-getWeather();
+function getRequiredData(data)
+{
+    const address=data.address;
+    const condition=data.currentConditions;
+    const day=data.days[0];
+    return {address,condition,day};
+}
+//main
+let data;
+getWeather().then(function(fullData){
+    data=getRequiredData(fullData);
+    console.log(data.condition);
+});
