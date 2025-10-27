5199a9bf7a84567490cbc5cee427761a1b002f7e
singhutp-glitch <singhutp@gmail.com>
2025-10-27 12:10:02 +0530

    Get basic api call

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles.css";
 console.log("hello");
+
+async function getWeather()
+{
+    const response=await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=3F26HRSJQUHVH5X589RRYHJVR');
+    console.log(response);
+}
+getWeather();
