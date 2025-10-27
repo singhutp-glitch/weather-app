36497a4316bf843a61dadbe48aec3dfa1f643064
singhutp-glitch <singhutp@gmail.com>
2025-10-27 13:00:40 +0530

    Add basic search box working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./styles.css";
 console.log("hello");
 
-async function getWeather()
+async function getWeather(location)
 {
-    const response=await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=3F26HRSJQUHVH5X589RRYHJVR');
+    const response=await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+location+'?key=3F26HRSJQUHVH5X589RRYHJVR');
     const data=await response.json();
     return data;
 }
@@ -16,13 +16,24 @@ function getRequiredData(data)
 }
 //main
 let data;
-getWeather().then(function(fullData){
+getWeather('london').then(function(fullData){
     data=getRequiredData(fullData);
-    console.log(data.condition);
+    console.log(data.condition.conditions);
 });
 const form=document.querySelector('form');
 const searchBox=form.querySelector('input');
 const searchBtn=form.querySelector('button');
 searchBtn.addEventListener('click',function(){
-    console.log(searchBox.value);
+    const location=searchBox.value;
+    if(location==="")
+    {
+        alert('put some location first');
+    }
+    else
+    {
+        getWeather(location).then(function(fullData){
+        data=getRequiredData(fullData);
+        console.log(data.condition.conditions);
+});     
+    }
 })
