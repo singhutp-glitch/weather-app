1420a234344d50508d3bcc3575000a5225739bf2
singhutp-glitch <singhutp@gmail.com>
2025-10-27 12:23:02 +0530

    Show weather description

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ console.log("hello");
 async function getWeather()
 {
     const response=await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=3F26HRSJQUHVH5X589RRYHJVR');
-    console.log(response);
+    const data=await response.json();
+    console.log(data.description);
 }
 getWeather();
